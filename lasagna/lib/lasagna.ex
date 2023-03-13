a2c0119397b5fd2c4b312d4235820b4184891da7
Alex Ghoust <ghoust@pinacotheca.net>
2023-03-13 15:12:29 +0600

    Add lasagna

---
+++ lasagna/lib/lasagna.ex
@@ -0,0 +1,21 @@
+defmodule Lasagna do
+  def expected_minutes_in_oven do
+    40
+  end
+
+  def remaining_minutes_in_oven(t) do
+    expected_minutes_in_oven() - t
+  end
+
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
+
+  def total_time_in_minutes(layers, t) do
+    t + preparation_time_in_minutes(layers)
+  end
+
+  def alarm do
+    "Ding!"
+  end
+end
